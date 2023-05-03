a98341b61da16d8b10843a816849d8ea8e1ca14a
Admin <Admin@DESKTOP-IHN1TPT>
2023-05-03 14:28:12 +0800

    sdfgh

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		 Course course = new Course ("Software Construction");
+		 course.setCourseName("Software Construction");
+		 
+		// System.out.println(course1.CourseName());
+		 System.out.println(course.getCourseName());
+		 
+		 course.setCourseName("Программ хангамжийн бүтээлт");
+		 System.out.print(course.getCourseName());
+	}
+
+}
